5cef2696af3da750a811ee81deaa2a8cb4803d72
Zoulei1999 <91334688+Zoulei1999@users.noreply.github.com>
2022-04-26 11:43:18 +0100

    Final Version
    Lab test for GE2 2022

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -2,18 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Nematode : MonoBehaviour
-{
-    public int length = 5;
+public class Nematode : MonoBehaviour{
+    
+    public int length;
 
     public Material material;
 
-    void Awake()
-    {
+    private Vector3 segmentSize;
+
+    void Awake(){
         // Put your code here!
+        length = Random.Range(3, 30);
+        segmentSize = new Vector3(0.1f, 0.1f, 0f);
+
+        for (var i = 0; i < length; i++){//loop for the printing of segments
+            
+            GameObject segments = GameObject.CreatePrimitive(PrimitiveType.Sphere); //prints spheres
+
+            segments.transform.parent = this.transform;
+            segments.transform.position = transform.position - (transform.forward * i);//setting position
+            segments.transform.rotation = transform.rotation;//setting rotation
+            segments.transform.localScale -= segmentSize;
+
+            segments.GetComponent<Renderer>().material = material;//rendering nematode material
+            segments.GetComponent<Renderer>().material.color = Color.HSVToRGB(i / (float)length, 1, 1);//setting colors
+
+            if (i > length/2){//checks if the segment is greater than halfway to either increase or decrease size of the segments
+                
+                segmentSize -= new Vector3(-0.1f, -0.1f, -0);   
+            
+            } else {
+            
+                segmentSize -= new Vector3(0.1f, 0.1f, 0);   
+            
+            }
+        }
+
+        GameObject parent = this.transform.GetChild(0).gameObject; // setting parent object
+        parent.AddComponent<Boid>();//adding boid behaviour
+        parent.AddComponent<NoiseWander>();//adding noise wander behaviour
+        parent.AddComponent<ObstacleAvoidance>();//obstacle avoidance behaviour
+    
     }
-
-
+    
     // Start is called before the first frame update
     void Start()
     {
--- GE2 Test 2022 Starter Unity/Assets/NematodeSchool.cs
+++ GE2 Test 2022 Starter Unity/Assets/NematodeSchool.cs
@@ -15,7 +15,23 @@ public class NematodeSchool : MonoBehaviour
     void Awake()
     {
         // Put your code here
+        Vector3 center = transform.position;
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 pos = Circle(center, radius);
+            Quaternion rot = Quaternion.FromToRotation(Vector3.forward, center-pos);
+            Instantiate(prefab, pos, rot);       
+        }
     }
+
+    Vector3 Circle ( Vector3 center, int radius){
+         float ang = Random.value * 360;
+         Vector3 pos;
+         pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+         pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+         pos.z = center.z;
+         return pos;
+     }
 
     // Update is called once per frame
     void Update()
